387ae086273faaa86957f6fb0421f0b05836182e
mokotan1 <103185768+mokotan1@users.noreply.github.com>
2025-09-30 21:01:12 +0900

    챗봇 강화 + 상호작용 처리

--- disputatio/Assets/mokotan/mokotan/script/KitchenChatbot.cs
+++ disputatio/Assets/mokotan/mokotan/script/KitchenChatbot.cs
@@ -81,7 +81,7 @@ public class KitchenChatbot : MonoBehaviour
             bool giveFood = kitchenFlowchart.GetBooleanVariable("giveFood");
             if (giveFood)
             {
-                finalSystemPrompt += "\n\n[중요 지시] 플레이어는 당신에게 먹이를 주었습니다. 그에 대한 감사로 '카레'에 대한 힌트를 수수께끼로 내주세요.";
+                finalSystemPrompt += "\n\n[중요 지시] 플레이어는 당신에게 먹이를 주었습니다. 그에 대한 감사로 '카레'에 대한 힌트를 수수께끼로 내주세요. 수수께끼는 내지만 음식이라는 것은 알 수 있도록 내주세요.";
             }
         }
 
